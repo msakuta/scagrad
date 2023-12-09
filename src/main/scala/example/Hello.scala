835f35772d2cd799d6c5f216eea03aba7ae2bff2
msakuta <masahiro.sakuta@gmail.com>
2023-12-09 20:07:06 +0900

    Add Mul type

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,34 +1,29 @@
 package example
 
-object Hello extends Greeting with App {
+object Hello extends App {
   def eval(term: Term): Double = term match {
     case Value(_, v) => v
     case Add(_, lhs, rhs) => eval(lhs) + eval(rhs)
+    case Mul(_, lhs, rhs) => eval(lhs) * eval(rhs)
   }
 
   def derive(term: Term, wrt: Term): Double = term match {
     case Value(id, _) => if (wrt == term) 1 else 0
     case Add(_, lhs, rhs) => derive(lhs, wrt) + derive(rhs, wrt)
+    case Mul(_, lhs, rhs) => derive(lhs, wrt) * eval(rhs) + eval(lhs) * derive(rhs, wrt)
   }
 
   var a = Value("a", 123)
   var b = Value("b", 123)
   var c = Value("c", 42)
   var ab = Add("ab", a, b)
-  var abc = Add("abc", ab, c)
+  var abc = Mul("abc", ab, c)
   println(eval(abc))
   println(derive(abc, a))
-  println(greeting)
 }
 
 sealed trait Term
 case class Value(id: String, a: Double) extends Term
 case class Add(id: String, lhs: Term, rhs: Term) extends Term
+case class Mul(id: String, lhs: Term, rhs: Term) extends Term
 
-trait Greeting {
-  lazy val greeting: String = "hello, world!!!"
-}
-
-// class Term {
-//   var type = 
-// }
